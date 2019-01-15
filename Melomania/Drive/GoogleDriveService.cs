7ba67ef4b0fa55fcfbe3046d48722a9eb59b62b4
dnikolovv <dobromir@devadventures.net>
2019-01-15 11:30:19 +0200

    Initial.

---
+++ Melomania/Drive/GoogleDriveService.cs
@@ -0,0 +1,36 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using System.IO;
+
+namespace Melomania.Drive
+{
+    public class GoogleDriveService
+    {
+        private static string[] Scopes = { DriveService.Scope.Drive };
+        private static string ApplicationName = "Melomania";
+
+        private readonly DriveService _driveService;
+
+        public UserCredential Authorize(string credentialsPath)
+        {
+            if (!File.Exists(credentialsPath))
+            {
+                throw new FileNotFoundException("Please provide a valid path to your credentials.json file (see https://developers.google.com/drive/api/v3/quickstart/dotnet for help).");
+            }
+
+            using (var stream = new FileStream(credentialsPath, FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+        }
+    }
+}
---
+++ Melomania/Program.cs
@@ -0,0 +1,71 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace Melomania
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Start
+            // Check if it has been started before and has google credentials/token saved
+            // Check if it has a google drive root folder set
+            // -- Provide functionality for changing both of the above
+            
+            // If it has been started for the first time it may display something like
+            // Please run "melomania setup" before trying to save any tracks
+
+            // "melomania setup" is going to save a config file (perhaps json) somewhere in AppData (think about Unix support)
+            UserCredential credential;
+
+            using (var stream = new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Drive API service.
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define parameters of request.
+            FilesResource.ListRequest listRequest = service.Files.List();
+            listRequest.PageSize = 10;
+            listRequest.Fields = "nextPageToken, files(id, name)";
+
+            // List files.
+            IList<Google.Apis.Drive.v3.Data.File> files = listRequest.Execute().Files;
+
+            Console.WriteLine("Files:");
+            if (files != null && files.Count > 0)
+            {
+                foreach (var file in files)
+                {
+                    Console.WriteLine("{0} ({1})", file.Name, file.Id);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No files found.");
+            }
+            Console.Read();
+        }
+    }
+}
